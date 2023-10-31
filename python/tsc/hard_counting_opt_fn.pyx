96a16ab29cf82fd6790db7b3cbc31724b292a3d6
jose <jose.pc.fonseca@gmail.com>
2023-10-31 08:05:47 +0100

    PERFORMANCE: added OpenMP to hard_counting function.

--- python/tsc/hard_counting_opt_fn.pyx
+++ python/tsc/hard_counting_opt_fn.pyx
@@ -20,10 +20,11 @@ def hard_counting_opt(cnp.ndarray[DTYPE_UINT32_t, ndim=3] max_idxs, unsigned int
 
     cdef cnp.ndarray[DTYPE_UINT32_t, ndim=3] feats = np.zeros([num_examples, num_groups, kernels_per_group], dtype=DTYPE_UINT32)
 
-    for ex in range(num_examples):
-        for gr in range(num_groups):
-            for s in range(num_samples):
-                idx = max_idxs[ex,gr,s]
-                feats[ex,gr,idx] += 1
+    with nogil:
+        for ex in prange(num_examples, schedule='static', num_threads=16):
+            for gr in range(num_groups):
+                for s in range(num_samples):
+                    idx = max_idxs[ex,gr,s]
+                    feats[ex,gr,idx] += 1
     
     return feats
